bfabed33450b18bf1c9d82cbadb8436cb23fff78
Debbie Natalia <debbyenatalia@gmail.com>
2026-05-24 12:47:33 +0200

    feature: actualizado app.xaml

--- ITVLuisVives.Front/App.xaml.cs
+++ ITVLuisVives.Front/App.xaml.cs
@@ -1,12 +1,28 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using ITVLuisVives.Front.ViewModels;
+using ITVLuisVives.Front.Views;
 
 namespace ITVLuisVives.Front;
 
 /// <summary>
-/// Interaction logic for App.xaml
+///     Punto de entrada de la aplicación WPF.
+///     Configura el ciclo de vida inicial de la interfaz y la vinculación del patrón MVVM.
 /// </summary>
 public partial class App : Application
 {
+    /// <summary>
+    ///     Método ejecutado al iniciar la aplicación.
+    ///     Instancia la ventana principal, inicializa su contexto de datos y la despliega.
+    /// </summary>
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        MainWindow mainWindow = new MainWindow();
+
+        MainWindowViewModel viewModel = new MainWindowViewModel();
+        mainWindow.DataContext = viewModel;
+
+        mainWindow.Show();
+    }
 }
